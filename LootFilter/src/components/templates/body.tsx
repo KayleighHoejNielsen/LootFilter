e25b0b214d0688d6f21066cd9636627b7c7aea89
Erik Nielsen <erik.nielsen1992@gmail.com>
2022-07-04 13:45:24 +0100

    Add in helper code to start writing functions

--- LootFilter/src/components/templates/body.tsx
+++ LootFilter/src/components/templates/body.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
+import { readCSV } from "../../helpers/fileHelper";
 import GenerateSection from "../organisms/generateSection";
 
 export default class Body extends React.Component<{}> {
     render() {
+        readCSV("test.csv");
+
         return (
             <div>
                 <h3>A Simple React Component Example with Typescript</h3>
---
+++ LootFilter/src/helpers/fileHelper.ts
@@ -0,0 +1,3 @@
+export const readCSV = (filename: string): string => {
+
+}
